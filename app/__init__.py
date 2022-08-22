bd0aca70aa904281fe619e3eb3a7473792b6806a
Isabel <isabel@isabels-imac.attlocal.net>
2022-08-22 11:53:05 -0500

    note backend routes

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,9 @@ from flask_login import LoginManager
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
+# Isabel Edit 
+from .api.notes import note_routes
+# Isabel Edit
 
 from .seeds import seed_commands
 
@@ -31,6 +34,9 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
+# Isabel Edit
+app.register_blueprint(note_routes, url_prefix='/api')
+# Isabel Edit
 db.init_app(app)
 Migrate(app, db)
 
---
+++ app/api/notes.py
@@ -0,0 +1,79 @@
+from flask import Blueprint, jsonify, session, request, redirect
+from app.models import Post ,Note, db
+from app.forms.note_form import NoteForm
+from flask_login import current_user, login_user, logout_user, login_required
+
+note_routes = Blueprint('notes', __name__, "")
+
+# get all notes DONE
+@note_routes.route("/note/all")
+def get_all_post():
+    notes = Note.query.all()
+    response = [note.to_dict() for note in notes ]
+    return {'note': response}
+
+# get all notes by post id  DONE
+@note_routes.route('/posts/<int:post_id>/notes')
+def one_note(post_id):
+    note = Note.query.get(post_id)
+    response = [note.to_dict()]
+    return {'note': response}
+
+# delete note by id DONE 
+@note_routes.route("/notes/<int:id>", methods=["Delete"])
+@login_required
+def make_note(id):
+        note = Note.query.get(id)
+        if note.user_id != current_user.get_id():
+         redirect('api/auth/unauthorized')
+
+        db.session.delete(note)
+        db.session.commit()
+
+
+# post comment by post id
+@note_routes.route("/post/<int:post_id1>/notes", methods=["POST"])
+@login_required
+
+def post_note(post_id1):
+
+    post = Post.query.get(post_id1)
+
+    form = NoteForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        note1 = Note(
+            description=form.data['description'],
+            post_id=post.id,
+            user_id=current_user.get_id()
+        )
+        
+        db.session.add(note1)
+        db.session.commit()
+        response = [note1.to_dict()]
+        return {'note': response }
+
+    return {'errors': form.errors}, 401
+
+
+#edit note by comment id
+@note_routes.route('/note/<int:id>', methods=['PUT'])
+@login_required
+def edit_post(id):
+    note = Note.query.get(id)
+
+    if note.user_id != current_user.get_id():
+        redirect('api/auth/unauthorized')
+
+    form = NoteForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        note.description=form.data['description']
+
+
+        db.session.commit()
+        return note.to_dict()
+
+    return {'errors': form.errors}, 401
