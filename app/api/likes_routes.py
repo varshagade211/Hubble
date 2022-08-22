93372217f1855db42f47663c2efe91309c583cf6
GrantChristopherson <oh.grant.christopherson@gmail.com>
2022-08-22 10:07:55 -0700

    updating to prepare for main pull

--- app/api/likes_routes.py
+++ app/api/likes_routes.py
@@ -7,28 +7,49 @@ likes_routes = Blueprint('likes', __name__)
 
 
 
+
 @likes_routes.route('/')
 @login_required
-def users_all_post_likes():
-  user = User.query(User.id == current_user.id)
-  return {'likes':[like.to_]}
-  # user = User.query.filter(User.id == current_user.id)
-  # return {'likes':[like.to_dict() for like in user.user_likes]}
+def user_likes():
+  posts = Post.query.join(User).filter(User.id == current_user.id)
+  return {'posts':post.to_dict() for post in posts}
 
 
 
-  # return {'likes':[post.to_dict() for post in current_user.user_likes]}
-
-  # user = User.query.filter(User.id == current_user.id)
-  # likes = user.users_likes
-  # return {'likes':[like.to_dict() for like in likes]}
-# current_user.users_likes
-  
 
 
-  # posts = Post.query.filter(like for like in Post.posts_likes if like == current_user).all()
-  # print(posts)
-  # return {'likes':[like.to_dict() for like in posts]}
-  
 
+
+
+# @likes_routes.route('/')
+# def user_likes():
+ 
+# @likes_routes.route('/')
+# # @login_required
+# def user_likes():
+#   posts = Post.query.join(Post, User).all()
+#   return {'posts':post.to_dict() for post in posts}
+
+# @likes_routes.route('/')
+# # @login_required
+# def user_likes():
+#   liked_posts = User.query.select_from(User.id).join(Post, Post.id)
+#   return {'likes':[like.to_dict() for like in liked_posts]}
+
+
+# def users_all_post_likes():
+#   posts = User.query.filter([post for post in User.user_likes if User.id == current_user.id]).all()
+#   return {'likes':[post.to_dict() for post in posts]}
+
+# user = User.query.filter(User.id == current_user.id)
+# return {'likes':[like.to_dict() for like in user.user_likes]}
+
+
+# user = User.query.filter(User.id == current_user.id)
+# likes = user.users_likes
+# return {'likes':[like.to_dict() for like in likes]}
+
+# posts = Post.query.filter(like for like in Post.posts_likes if like == current_user).all()
+# print(posts)
+# return {'likes':[like.to_dict() for like in posts]}
 
