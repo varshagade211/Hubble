140b6d48e43bc208a686a1c44d9e7fd3776e7cb7
Isabel <isabel@isabels-imac.attlocal.net>
2022-08-25 13:03:22 -0500

    merge conflix

--- react-app/src/store/note.js
+++ react-app/src/store/note.js
@@ -137,29 +137,19 @@ const notesReducer = (state = initialState, action) => {
         }
 
         case EDIT: {
-           state?.notes?.forEach((note, i)=>{
+           
 
-<<<<<<< HEAD
            
            state?.notes?.forEach((note, i)=>{ 
             
-=======
->>>>>>> main
             if(note?.id === action?.updatedComment?.id)
 
             state?.notes?.splice(i, 1, action?.updatedComment)
             })
-<<<<<<< HEAD
     
            newState = {...state, notes:[...state?.notes]}
            newState[action?.updatedComment?.id] = action?.updatedComment
            
-=======
-        //    always do line 155 BEFORE line 156 to return updated state
-        newState = {...state, notes:[...state?.notes]}
-        newState[action?.updatedComment?.id] = action?.updatedComment
-
->>>>>>> main
            return newState
         }
 
