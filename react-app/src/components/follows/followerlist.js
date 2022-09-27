d142f888a939dedd11b8c06cf99e93d795a56aa9
reneeluo7 <rong_2_u@hotmail.com>
2022-09-27 14:06:25 -0700

    working on followerlist

--- react-app/src/components/follows/followerlist.js
+++ react-app/src/components/follows/followerlist.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserFollowers } from "../../store/follows";
+import {
+  addFollowingThunk,
+  updateUnfollowed,
+} from "../../store/follows";
 import "./followerlist.css";
 
 import SideBar from '../SideBar';
@@ -11,12 +15,14 @@ function FollowerList() {
 //   const { id } = useParams();
   const dispatch = useDispatch();
   const user = useSelector((state) => state?.session?.user);
-
+  // const unfollowList = useSelector(state => Object.values(state.follows.unfollowed))
+  // console.log(unfollowList)
   const followers = useSelector((state) => state?.follows?.followers
   );
+  const [isLoaded, setIsLoaded] = useState(false)
 
   useEffect(() => {
-    dispatch(getUserFollowers(user.id));
+    dispatch(getUserFollowers(user.id)).then(() => setIsLoaded(true));
   }, [dispatch, user.id]);
 
   return (
@@ -44,6 +50,9 @@ function FollowerList() {
                   {user?.username}
                 </Link>
               </div>
+             { <div className="follow-btn" >
+                <FollowClick listeduser={user} />
+              </div>}
 
             </div>
           </div>
@@ -58,5 +67,26 @@ function FollowerList() {
   );
 }
 
+const FollowClick = (listeduser) => {
+
+
+  const dispatch = useDispatch();
+  const current_user = useSelector((state) => state?.session?.user);
+
+
+  const handleFollowing = async (e) => {
+    e.preventDefault();
+    dispatch(addFollowingThunk(current_user?.id, listeduser?.listeduser?.id));
+    dispatch(updateUnfollowed(listeduser?.listeduser?.id));
+  };
+
+  return (
+    <>
+      <button key={Math.random()} onClick={handleFollowing}>
+        Follow
+      </button>
+    </>
+  );
+};
 
 export default FollowerList;
