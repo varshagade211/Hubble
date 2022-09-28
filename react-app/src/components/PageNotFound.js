f0f0ecf5de1752e32f1be4e22aee3eeea6b7d4a8
reneeluo7 <rong_2_u@hotmail.com>
2022-09-27 22:11:53 -0700

    add 404 page

---
+++ react-app/src/components/PageNotFound.js
@@ -0,0 +1,12 @@
+import './PageNotFound.css';
+
+const NotFound = () => {
+    return(
+        <div className='not-found-page'>
+            <h2>404 <br></br> Page Not Found</h2>
+            <div>Please go back!</div>
+        </div>
+    )
+}
+
+export default NotFound
